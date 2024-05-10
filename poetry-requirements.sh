d63f84017938b742ec7507109b66fa981e440e3c
Jazo Madrid <jazo.madrid@aelogica.com>
2024-05-09 21:49:23 -0600

    Local requirements depend on production

--- poetry-requirements.sh
+++ poetry-requirements.sh
@@ -4,11 +4,11 @@ command -v poetry >/dev/null 2>&1 || { echo >&2 "I require poetry but it's not i
 
 poetry export --without-hashes -n --without-urls| cut -f1 -d";" | sed 's/ *$//g' > requirements/base.txt
 
-echo /dev/null > requirements/local.txt
-echo "-r base.txt\n" > requirements/local.txt
-poetry export --without-hashes --only dev -n --without-urls| cut -f1 -d";" | sed 's/ *$//g' >> requirements/local.txt
-
-
 echo /dev/null > requirements/production.txt
 echo "-r base.txt\n" > requirements/production.txt
 poetry export --without-hashes --only prod -n --without-urls| cut -f1 -d";" | sed 's/ *$//g' >> requirements/production.txt
+
+
+echo /dev/null > requirements/local.txt
+echo "-r production.txt\n" > requirements/local.txt
+poetry export --without-hashes --only dev -n --without-urls| cut -f1 -d";" | sed 's/ *$//g' >> requirements/local.txt
