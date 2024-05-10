18bd4898332ea1eb6a82ec6bf1b61f74d63a0f94
Jazo Madrid <jazo.madrid@aelogica.com>
2024-05-09 21:43:55 -0600

    Added script to create requirements

---
+++ poetry-requirements.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+command -v poetry >/dev/null 2>&1 || { echo >&2 "I require poetry but it's not installed. Aborting."; exit 0; }
+
+poetry export --without-hashes -n --without-urls| cut -f1 -d";" | sed 's/ *$//g' > requirements/base.txt
+
+echo /dev/null > requirements/local.txt
+echo "-r base.txt\n" > requirements/local.txt
+poetry export --without-hashes --only dev -n --without-urls| cut -f1 -d";" | sed 's/ *$//g' >> requirements/local.txt
+
+
+echo /dev/null > requirements/production.txt
+echo "-r base.txt\n" > requirements/production.txt
+poetry export --without-hashes --only prod -n --without-urls| cut -f1 -d";" | sed 's/ *$//g' >> requirements/production.txt
